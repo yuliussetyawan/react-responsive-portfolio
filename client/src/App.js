ec7b6dec20482901d9539640f0504131db3c1ea5
yuliussetyawan <yuliusetyawan@gmail.com>
2024-02-23 23:34:59 +0700

    Part-1 Create Project and Install Bootstrap && Part-2 Sidebar Section File Created

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./Components/Sidebar/Sidebar";
+
+const App = () => {
+  return (
+    <div className="container-fluid sidebar-section">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default App;
