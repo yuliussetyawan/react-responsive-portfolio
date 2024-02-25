502bdedbd1c99a5f908cfcf8f766244ca901fbd6
yuliussetyawan <yuliusetyawan@gmail.com>
2024-02-25 21:43:31 +0700

    Part-3 Sidebar Expand and Collapse Features

--- client/src/Components/Sidebar/Sidebar.js
+++ client/src/Components/Sidebar/Sidebar.js
@@ -1,9 +1,32 @@
-import React from 'react'
-import './Sidebar.css'
+import React, { useState } from "react";
+import "./Sidebar.css";
+import Home from "../Home/Home";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 const Sidebar = () => {
+  const [expandSidebar, setExpandSidebar] = useState(true);
+  const handleExpandClick = () => {
+    setExpandSidebar((expandSidebar) => !expandSidebar);
+  };
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="sidebar-section container-fluid">
+      <div className={expandSidebar ? "sidebar sidebar-expand" : "sidebar"}>
+        <div className="icon-for-sidebar-expand-and-collapse">
+          <p onClick={handleExpandClick}>
+            {expandSidebar ? (
+              <BsChevronLeft size={30} />
+            ) : (
+              <BsChevronRight size={30} />
+            )}
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <p>
+          <Home />
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
