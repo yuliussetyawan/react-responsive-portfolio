0fd49afdb61291234637ffca30a0341dcdf85c33
yuliussetyawan <yuliusetyawan@gmail.com>
2024-02-26 22:59:38 +0700

    Part-5 Sidebar Navbar Item List Design

--- client/src/Components/Sidebar/SidebarList.js
+++ client/src/Components/Sidebar/SidebarList.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./SidebarList.css";
 import profilepic from "../../Image/5556468.png";
+import {
+  FcNightPortrait,
+  FcHome,
+  FcTodoList,
+  FcContacts,
+  FcFactory,
+  FcSalesPerformance,
+} from "react-icons/fc";
+
+import { MdBiotech } from "react-icons/md";
+import { MdCastForEducation } from "react-icons/md";
 
 const SidebarList = ({ expandSidebar }) => {
   return (
@@ -10,9 +21,62 @@ const SidebarList = ({ expandSidebar }) => {
           <div className="sidebar-profile-pic">
             <img src={profilepic} alt="profile-pic" />
           </div>
+          <ul>
+            <li className="nav-item">
+              <FcHome size={25} /> Home
+            </li>
+            <li className="nav-item">
+              <FcNightPortrait size={25} /> About
+            </li>
+            <li className="nav-item">
+              <FcFactory size={25} /> Work Experience
+            </li>
+            <li className="nav-item">
+              <MdBiotech size={25} color="orange" /> Tech Stack
+            </li>
+            <li className="nav-item">
+              <MdCastForEducation size={25} color="yellow" /> Education
+            </li>
+            <li className="nav-item">
+              <FcTodoList size={25} /> Projects
+            </li>
+            <li className="nav-item">
+              <FcSalesPerformance size={25} /> Testimonial
+            </li>
+            <li className="nav-item">
+              <FcContacts size={25} /> Contact
+            </li>
+          </ul>
         </div>
       ) : (
-        <div></div>
+        <div className="navbar-items-only-icons">
+          <ul>
+            <li className="nav-item">
+              <FcHome size={25} />
+            </li>
+            <li className="nav-item">
+              <FcNightPortrait size={25} />
+            </li>
+            <li className="nav-item">
+              <FcFactory size={25} />
+            </li>
+            <li className="nav-item">
+              <MdBiotech size={25} color="orange" />
+            </li>
+            <li className="nav-item">
+              <MdCastForEducation size={25} color="yellow" />
+            </li>
+            <li className="nav-item">
+              <FcTodoList size={25} />
+            </li>
+            <li className="nav-item">
+              <FcSalesPerformance size={25} />
+            </li>
+            <li className="nav-item">
+              <FcContacts size={25} />
+            </li>
+          </ul>
+        </div>
       )}
     </React.Fragment>
   );
