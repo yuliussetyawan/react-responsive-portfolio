481928d6528c80ab173bbb4edeabc4c39a49fd73
yuliussetyawan <yuliusetyawan@gmail.com>
2024-02-27 22:56:29 +0700

    Part-7 Home Section Typewriting Effect

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Home.css";
+import Typewriter from "typewriter-effect";
+
 const Home = () => {
   return (
     <div className="container-fluid home">
       <div className="container home-content">
-        <h1>Hi I'm a</h1>
+        <h1>Hi I'm Yulius</h1>
+        <h3>
+          <Typewriter
+            options={{
+              strings: [
+                "Full Stack Web Developer",
+                "MERN Stack Developer",
+                "UI/UX Designer",
+              ],
+              autoStart: true,
+              loop: true,
+              delay: 5,
+            }}
+          />
+        </h3>
       </div>
     </div>
   );
