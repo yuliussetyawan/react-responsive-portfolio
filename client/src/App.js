081023c169e9f63b0110eb0088d06f0abf8b4904
yuliussetyawan <yuliusetyawan@gmail.com>
2024-02-28 23:45:51 +0700

    Part-9 About Section Design

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import About from "./Components/About/About";
 
 const App = () => {
   return (
-    <div className="container-fluid sidebar-section">
+    <div>
       <Sidebar />
+      <About/>
     </div>
   );
 };
---
+++ client/src/Components/About/About.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./About.css";
+import ProfilePic from "../../Image/5556468.png";
+
+const About = () => {
+  return (
+    <div className="container about-section">
+      <div className="row">
+        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+          <div className="about-image">
+            <img src={ProfilePic} alt="Profile" />
+          </div>
+        </div>
+        <div className="col-xl-6 col-lg-6 col-sm-12 col-md-12">
+          <div className="about-details">
+            <div className="about-title">
+              <h5>About Me</h5>
+              <span className="line"></span>
+            </div>
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using 'Content here,
+              content here', making it look like readable English. Many desktop
+              publishing packages and web page editors now use Lorem Ipsum as
+              their default model text, and a search for 'lorem ipsum' will
+              uncover many web sites still in their infancy. Various versions
+              have evolved over the years, sometimes by accident, sometimes on
+              purpose (injected humour and the like).
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
