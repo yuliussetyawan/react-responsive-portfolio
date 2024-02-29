27ee2b72df09a2fe35a80ab3b81ecf3d9093cf53
yuliussetyawan <yuliusetyawan@gmail.com>
2024-02-29 14:52:58 +0700

    Part-11 Random Color Generate for Tech Stack Item

--- client/src/Components/TechStack/TechStack.js
+++ client/src/Components/TechStack/TechStack.js
@@ -61,6 +61,24 @@ const data = [
   },
 ];
 
+const colors = [
+  "#F1C40F",
+  "#4B088A",
+  "#8181F7",
+  "#FE2EF7",
+  "#585858",
+  "#800000",
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#001CCE",
+  "#00C79F",
+  "#FFBB24",
+  "#FF1042",
+  "#FF1042",
+];
+
 const TechStack = () => {
   return (
     <div className="container tech-stack-section">
@@ -69,9 +87,15 @@ const TechStack = () => {
         <span className="line"></span>
       </div>
       <div className="row">
-        {data.map((item) => (
+        {data.map((item, index) => (
           <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12" key={item.id}>
             <div className="tech-content">
+              <span
+                className="tech-number"
+                style={{ backgroundColor: colors[index] }}
+              >
+                {index + 1}
+              </span>
               <p>{item.name}</p>
             </div>
           </div>
