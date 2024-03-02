2e317d980c16f239bfb866d2b60468f9023d8cbd
yuliussetyawan <yuliusetyawan@gmail.com>
2024-03-02 14:58:44 +0700

    Part-14 Project Section API Design

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import About from "./Components/About/About";
 import TechStack from "./Components/TechStack/TechStack";
+import Project from "./Components/Projects/Project";
 
 const App = () => {
   return (
     <div>
       <Sidebar />
-      <About/>
-      <TechStack/>
+      <About />
+      <TechStack />
+      <Project />
     </div>
   );
 };
