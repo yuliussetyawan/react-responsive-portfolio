7dd20258f9b70fbf73a46b271e841be23ec534b4
yuliussetyawan <yuliusetyawan@gmail.com>
2024-03-05 20:23:17 +0700

    Part-22 Implement Slick Slider to Show Testimonial Data

--- client/src/Components/Testimonial/Testimonial.js
+++ client/src/Components/Testimonial/Testimonial.js
@@ -59,11 +59,65 @@ const data = [
 ];
 
 const Testimonial = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    initialSlide: 0,
+    autoplay: true,
+    rows: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <div className="container testimonial-section">
       <div className="section-title">
         <h5>Testimonial</h5>
         <span className="line"></span>
+      </div>
+
+      <div className="testimonial-slider">
+        <Slider {...settings}>
+          {data.map((item, index) => (
+            <div className="content-slider-main">
+              <div className="content-slider" key={index}>
+                <img
+                  src={item.img}
+                  alt="testimonial-img"
+                  className="center-image"
+                />
+                <p>{item.name}</p>
+                <p>{item.position}</p>
+                <p>{item.des}</p>
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
